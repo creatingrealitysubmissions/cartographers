ceac4e8b33163f968b385bc951e4e03c6e528cd9
Harrison Leon <hleon@usc.edu>
2018-03-14 17:47:07 -0700

    THE DOOR

--- thedoorvr/Assets/Scripts/OpenDoor.cs
+++ thedoorvr/Assets/Scripts/OpenDoor.cs
@@ -24,14 +24,8 @@ public class OpenDoor : MonoBehaviour {
 			// vector to moving handle
 			vectorB = new Vector2 (doorRotator.transform.position.x, doorRotator.transform.position.y) - new Vector2 (handleController.transform.position.x, handleController.transform.position.y);
 
-			float newAngle = (Vector2.Angle (vectorA, vectorB)) * 1.3f;
+			float newAngle = -(Vector2.Angle (vectorA, vectorB) + 50.0f) * 1.3f;
 
-			if (newAngle - 100.0f < 20.0f) {
-				// play door close sound
-				newAngle = 100.0f;
-			} else if (newAngle - 100.0f > 70.0f) {
-				newAngle = 190.0f;
-			}
 
 
 			doorRotator.transform.localRotation = Quaternion.Euler (0.0f, newAngle - 100.0f, 0.0f);
